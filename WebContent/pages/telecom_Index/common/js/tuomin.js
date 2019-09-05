d664e182f7f10cef0f69708fd9d42d9ba686f2a0
xhcf <formatc@yeah.net>
2019-09-05 15:04:40 +0800

    增加脱敏内容

--- WebContent/pages/telecom_Index/common/js/tuomin.js
+++ WebContent/pages/telecom_Index/common/js/tuomin.js
@@ -1,6 +1,22 @@
 var tuomin_addr = true;//地址是否脱敏，若个别页面不需要脱敏，则在具体页面中申明此变量为false即可
+var tuomin_phone = true;
 function addr(val){
+    if(val==null || val==undefined)
+        return "";
     if(tuomin_addr)
-        return val.replace(/[0-9]/ig,'*');
+        return val.replace(/[0-9]/ig,'*').replace(/[\uFF10-\uFF19]/ig,'*');
     return val;
+}
+function phoneHide(phone){
+    if(phone==null || phone==undefined)
+        return "";
+    if(tuomin_phone){
+        var d = phone.substr(0,3);
+        for(var i = 0,l = phone.length-5;i<l;i++){
+            d += "*";
+        }
+        return d+phone.substr(-2);
+    }else{
+        return phone;
+    }
 }

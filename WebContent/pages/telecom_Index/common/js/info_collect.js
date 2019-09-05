cc1cf1d0fe797f0a1c4a07f4fa88ba54429f45b6
xhcf <formatc@yeah.net>
2019-09-05 15:04:17 +0800

    去除无效变量

--- WebContent/pages/telecom_Index/common/js/info_collect.js
+++ WebContent/pages/telecom_Index/common/js/info_collect.js
@@ -100,10 +100,10 @@ $(function(){
     openNewWinInfoCollectEdit = function (segment_id,tab_id) {
         if(tab_id==undefined)
             tab_id = 0;
-        $.post(appBase+"/pages/telecom_Index/common/sql/viewPlane_custom_action.jsp",{"eaction":"prod_list","segment_id":segment_id,'acct_month':'${user_list_m_last_month.VAL}'},function(data){
+        $.post(appBase+"/pages/telecom_Index/common/sql/viewPlane_custom_action.jsp",{"eaction":"prod_list","segment_id":segment_id},function(data){
             var d = $.parseJSON(data);
             if(d.length>1){
-                $("#prod_list_win > iframe").attr("src", appBase+"/pages/telecom_Index/sub_grid/viewPlane_prod_list.jsp?segment_id=" + segment_id + "&city_id=" + city_id+"&acct_month="+'${user_list_m_last_month.VAL}');
+                $("#prod_list_win > iframe").attr("src", appBase+"/pages/telecom_Index/sub_grid/viewPlane_prod_list.jsp?segment_id=" + segment_id + "&city_id=" + city_id);
                 prod_list_win = layer.open({
                     title: '客户列表',
                     //title:false,
